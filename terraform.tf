fbb7e612e0a1f88a1c2b66e12d8195464fca2198
sandeep03422 <143420018+sandeep03422@users.noreply.github.com>
2023-12-02 17:52:05 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI, change to your desired AMI
   instance_type = "t2.micro"
-  key_name      = "your-key-pair-name"  # Change this to your key pair name
+  key_name      = "Devops"  # Change this to your key pair name
 
   tags = {
     Name = "example-instance"
